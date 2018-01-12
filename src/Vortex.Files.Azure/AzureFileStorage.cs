31683f1f3d64eb06d3cc8bf7df4a88892ba6a92f
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2018-01-12 17:18:24 -0500

    WIP azure file storage, first two tests and basic behavior added.

---
+++ src/Vortex.Files.Azure/AzureFileStorage.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilaterus.Vortex.Services.AzureStorage
+{
+    public class AzureFileStorage : IFileStorage
+    {
+        private AzureStorageConfig Configuration;
+        private CloudStorageAccount StorageAccount;
+        private CloudBlobClient BlobClient;
+        private CloudBlobContainer Container;
+
+        public AzureFileStorage(IOptions<AzureStorageConfig> configuration)
+        {
+            Configuration = configuration.Value;
+
+            StorageAccount = CloudStorageAccount.Parse(
+                Configuration.ConnectionString);
+
+            BlobClient = StorageAccount.CreateCloudBlobClient();
+            Container = BlobClient.GetContainerReference(Configuration.ContainerName);
+        }
+
+        public async Task<bool> DeleteFileAsync(string path)
+        {
+            CloudBlockBlob blockBlob = Container.GetBlockBlobReference(GetFileNameFromPath(path));
+            return await blockBlob.DeleteIfExistsAsync();
+        }
+
+        public string GenerateFileName()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+        public async Task<string> StoreFileAsync(Stream stream, string extension)
+        {
+            string fileName = GenerateFileName() + extension; ;
+            CloudBlockBlob blockBlob = Container.GetBlockBlobReference(fileName);
+
+            await blockBlob.UploadFromStreamAsync(stream);
+
+            return blockBlob.Uri.ToString();               
+        }
+
+        public static string GetFileNameFromPath(string path)
+        {
+            if(path == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var splitedPath = path.Split('/');
+            return splitedPath[splitedPath.Length - 1];
+        }
+    }
+}
---
+++ test/Vortex.Files.Azure.Tests/AzureFileStorageTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Equilaterus.Vortex.Services.AzureStorage.Tests
+{
+    public class AzureFileStorageTests
+    {
+        static IOptions<AzureStorageConfig> GetDefaultOptions()
+        {
+            AzureStorageConfig config = new AzureStorageConfig() {
+                ConnectionString = "UseDevelopmentStorage=true",
+                ContainerName = "test" };
+            return Options.Create(config);
+        }
+
+        [Fact]
+        public async Task AFSTests_StoreFile()
+        {
+            string fileName = "";
+            AzureFileStorage service = new AzureFileStorage(GetDefaultOptions());
+
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes("whatever")))
+            {
+                fileName = await service.StoreFileAsync(memoryStream, ".txt");
+            }
+
+            Assert.True(fileName != null && fileName.Length > 0);
+
+            var result = await service.DeleteFileAsync(fileName);
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void AFSTests_FileNameFromPathNull()
+        {
+            string path = null;
+            Assert.Throws<ArgumentNullException>(() => AzureFileStorage.GetFileNameFromPath(path));
+        }
+    }
+}
