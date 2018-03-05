81582eb4acbd2f24d177a2c225d21eb3a1b2d7b8
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-03-05 13:59:37 -0500

    Extended Persistance Manager (WIP)

---
+++ src/Vortex/Managers/AdjuntablePersistanceManager.cs
@@ -0,0 +1,36 @@
+﻿using Equilaterus.Vortex.Engine;
+using Equilaterus.Vortex.Engine.Queries;
+using Equilaterus.Vortex.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilaterus.Vortex.Managers
+{
+    public static class AdjuntablePersistanceManager
+    {
+        public static async Task<List<T>> InsertEntity<T>(
+            this PersistanceManager<T> p,
+            Stream stream)
+            where T : class, IAdjuntable
+        {
+            return await p.ExecuteCommand(
+                VortexEvents.InsertEntity, 
+                new VortexData(
+                    new RelationalQueryParams<T>()
+                    {
+                        Filter = filter,
+                        OrderBy = orderBy,
+                        Skip = skip,
+                        Take = take,
+                        IncludeProperties = includeProperties
+                    }
+                )
+            );
+        }
+    }
+}
---
+++ src/Vortex/Managers/RelationalPersistanceManager.cs
@@ -0,0 +1,36 @@
+﻿using Equilaterus.Vortex.Engine;
+using Equilaterus.Vortex.Engine.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilaterus.Vortex.Managers
+{
+    public static class RelationalPersistanceManager
+    {
+        public static async Task<List<T>> Find<T>(this PersistanceManager<T> p,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int skip = 0,
+            int take = 0,
+            params string[] includeProperties) where T : class
+        {
+            return await p.ExecuteQueryForEntities(
+                VortexEvents.RelationalQueryForEntities, 
+                new VortexData(
+                    new RelationalQueryParams<T>()
+                    {
+                        Filter = filter,
+                        OrderBy = orderBy,
+                        Skip = skip,
+                        Take = take,
+                        IncludeProperties = includeProperties
+                    }
+                )
+            );
+        }
+    }
+}
