0c6eaf3cf4d25128c450706036d2fcffa22bfeaa
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2018-03-05 14:22:26 -0500

    Local file storage WIP

---
+++ test/Vortex.Files.Local.Tests/LocalFileStorageTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equilaterus.Vortex.Services.LocalStorage.Tests
+{
+    public class LocalFileStorageTests
+    {
+    }
+}
