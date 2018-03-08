d83e4624e6b492f547d4ec438196e3d98bef21b0
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2018-03-08 12:07:26 -0500

    Softdelete command  and tests done

---
+++ src/Vortex/Engine/Commands/SoftDelete.cs
@@ -0,0 +1,28 @@
+﻿using Equilaterus.Vortex.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilaterus.Vortex.Engine.Commands
+{
+    public class SoftDelete<T> : GenericAction<T> where T : class, ISoftDeleteable
+    {
+        public override void Initialize()
+        {
+            IsDefaultAction = false;
+            PreventDefault = true;
+            Priority = 1;
+        }
+
+        public override async Task Execute()
+        {
+            Params.GetMainEntityAs<T>().IsDeleted = true;
+            await Context.DataStorage.UpdateAsync(Params.GetMainEntityAs<T>());
+        }
+
+        public SoftDelete(VortexContext<T> context) : base(context)
+        {            
+        }
+    }
+}
