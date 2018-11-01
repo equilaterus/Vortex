f8a019829264ccdfd6447a17731e126eb3d9d48c
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-11-01 00:04:25 -0500

    Extended reflection utils

---
+++ test/Vortex.Tests/Utilities/ExtendedReflection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Vortex.Tests.Utilities
+{
+    public static class ExtendedReflection
+    {
+        public static T GetPrivateField<T>(this object obj, string fieldName)
+        {
+            if (fieldName == null)
+            {
+                throw new ArgumentNullException(nameof(fieldName));
+            }
+
+            return (T)obj.GetType()
+                .GetField(
+                    fieldName,
+                    BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(obj);
+        }
+    }
+}
