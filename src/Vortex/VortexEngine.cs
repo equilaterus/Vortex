042bb91cf02c51665a70b754c88e99e765e9d589
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-12-10 19:28:18 -0500

    New VortexEngine (WIP)

--- src/Vortex/VortexEngine.cs
+++ src/Vortex/VortexEngine.cs
@@ -1,65 +1,112 @@
-﻿using System;
+﻿using Equilaterus.Vortex.Actions;
+using Equilaterus.Vortex.Filters;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Equilaterus.Vortex
 {
-    public class VortexEngine<T> : IVortexEngine<T> where T : class
+    public class VortexEngine<TEntity> : IVortexEngine<TEntity> where TEntity : class
     {
-        protected IVortexContext<T> _context;
+        protected IVortexContext<TEntity> _context;
 
         protected IVortexGraph _graph;
 
-        public VortexEngine() { }
+        protected IGenericFilterFactory _filterFactory;
 
-        public VortexEngine(IVortexGraph graph, IVortexContext<T> vortexContext)
+        public VortexEngine(IVortexGraph graph, IVortexContext<TEntity> vortexContext, IGenericFilterFactory filterFactory)
         {            
             _graph = graph;
             _context = vortexContext;
+            _filterFactory = filterFactory;
         }
 
-        public async Task<VortexData> RaiseEventAsync(string vortexEvent, VortexData actionParams)
-        {
-            List<VortexAction> actionsToExecute = new List<VortexAction>();
-            var actions = _graph.GetActions(vortexEvent, typeof(T));
-            foreach (var actionType in actions)
+        public async Task<TReturn> RaiseEventAsync<TReturn>(
+            string eventName, 
+            TEntity entity = null,
+            QueryParams<TEntity> queryParams = null, 
+            params object[] parameters)
+        {   
+            // TODO: REFACTOR!
+            // Prepare execution
+            List<VortexAction<TEntity>> actionsToExecute = new List<VortexAction<TEntity>>();
+            VortexReturnAction<TEntity, TReturn> returnAction = null;            
+
+            // Get bindings
+            var bindings = _graph.GetBindings(eventName, typeof(TEntity));
+            foreach (var binding in bindings)
             {
-                VortexAction vortexAction;
-                if (actionType.TypeOf.ContainsGenericParameters)
+                VortexAction<TEntity> vortexAction;
+
+                // Instantiate Vortex Actions from bindings
+                foreach (var bindedActionType in binding.Actions)
                 {
-                    Type[] typeArgs = { typeof(T) };
-                    var genericType = actionType.TypeOf.MakeGenericType(typeArgs);
-                    vortexAction = (VortexAction)Activator.CreateInstance(genericType, _context);
+                    if (!bindedActionType.ContainsGenericParameters)
+                    {
+                        vortexAction = (VortexAction<TEntity>)Activator.CreateInstance(bindedActionType, _context);
+                    }
+                    else
+                    {
+                        Type[] typeArgs = { typeof(TEntity) };
+                        var genericType = bindedActionType.MakeGenericType(typeArgs);
+                        vortexAction = (VortexAction<TEntity>)Activator.CreateInstance(genericType, _context);
+                    }
+                    actionsToExecute.Add(vortexAction);
+                }
+
+                // Keep first return action (if exists)
+                if (returnAction == null)
+                {
+                    if (binding.ReturnAction != null)
+                    {
+                        if (!binding.ReturnAction.ContainsGenericParameters)
+                        {
+                            returnAction = (VortexReturnAction<TEntity, TReturn>)Activator.CreateInstance(binding.ReturnAction, _context, _filterFactory);
+                        }
+                        else
+                        {
+                            Type[] typeArgs;
+                            if (binding.ReturnAction.GetGenericArguments().Count() == 1)
+                            {
+                                typeArgs = new Type[] { typeof(TEntity) };
+                            }
+                            // TODO: Discuss if we really need this:
+                            else
+                            {
+                                typeArgs = new Type[] { typeof(TEntity), typeof(TReturn) };
+                            }
+
+                            var genericType = binding.ReturnAction.MakeGenericType(typeArgs);
+                            returnAction = (VortexReturnAction<TEntity, TReturn>)Activator.CreateInstance(genericType, _context, _filterFactory);
+                        }
+                    }
+                }
+            }
+
+            // Execute!
+            foreach (var vortexAction in actionsToExecute)
+            {
+                await vortexAction.Execute(entity, parameters);
+            }
+
+            if (returnAction == null)
+            {
+                // TODO: Discuss this
+                if (queryParams != null)
+                {
+                    throw new Exception("Expected return action not found.");
                 }
                 else
                 {
-                    vortexAction = (VortexAction)Activator.CreateInstance(actionType.TypeOf, _context);
+                    return default;
                 }
-                vortexAction.Initialize();
-                vortexAction.Params = actionParams;
-                actionsToExecute.Add(vortexAction);
             }
-
-            actionsToExecute.Sort((VortexAction x, VortexAction y) => {
-                return y.Priority.CompareTo(x.Priority);
-            });
-
-            foreach (var vortexAction in actionsToExecute)
+            else
             {
-                await vortexAction.Execute();
-
-                if (vortexAction.IsReturnAction)
-                {
-                    return vortexAction.Results;
-                }
-
-                if (vortexAction.PreventDefault)
-                {
-                    break;
-                }
+                return await returnAction.Execute(queryParams, parameters);
             }
-            return null;
         }
     }
 }
