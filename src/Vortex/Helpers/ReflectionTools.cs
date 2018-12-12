746ebc7e33858bc29b0f52a816c2cf62995f8938
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-12-11 20:45:22 -0500

    Reflection tools supporting inheritance scenarios

---
+++ src/Vortex/Helpers/ReflectionTools.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Equilaterus.Vortex.Helpers
+{
+    public static class ReflectionTools
+    {
+        /// <summary>
+        /// Instantiates a Type with 0 or N generic parameters.
+        /// NOTES:
+        /// - args must correspond with a constructor overload of TType.
+        /// - Derived types with less Generic parameters are taken from left to right. 
+        ///   Ex: Query(T) : ReturnAction(T,R)
+        /// </summary>
+        /// <typeparam name="TReturn">Expected return type. Cannot contain undefined generic parameters.</typeparam>
+        /// <param name="type">Type to instantiate</param>
+        /// <param name="args">Contructor args</param>
+        /// <returns>Instance of TType</returns>
+        public static TReturn InstantiateAs<TReturn>(Type type, params object[] args) where TReturn : class
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (type.ContainsGenericParameters)
+            {
+                type = type.MakeGenericType(
+                    typeof(TReturn).GenericTypeArguments
+                        .Take(type.GetGenericTypeDefinition().GetGenericArguments().Count())
+                        .ToArray());
+            }
+
+            return Activator.CreateInstance(type, args) as TReturn;
+        }
+
+        /// <summary>
+        /// Instantiates a Type with 0 or N generic parameters.
+        /// NOTES:
+        /// - args must correspond with a constructor overload of TType.
+        /// - Derived types with less Generic parameters are taken from left to right. 
+        ///   Ex: Query(T) : ReturnAction(T,R)
+        /// </summary>
+        /// <typeparam name="TType">Expected return type. Cannot contain undefined generic parameters.</typeparam>
+        /// <param name="args">Contructor args</param>
+        /// <returns>Instance of TType</returns>
+        public static TType Instantiate<TType>(params object[] args) where TType : class
+        {
+            return InstantiateAs<TType>(typeof(TType), args);
+        }
+    }
+}
