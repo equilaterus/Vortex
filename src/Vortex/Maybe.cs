43e131104612d5e0ef42ed071f36dd589c2c0c1f
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-03-01 20:54:38 -0500

    Monads WIP

---
+++ src/Vortex/Maybe.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Equilaterus.Vortex
+{
+    public class Maybe<T>
+    {
+        private readonly bool _hasValue;
+        private readonly T _value;
+
+        public Maybe()
+        {
+            _hasValue = false;
+        }
+
+        public Maybe(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            _value = value;
+            _hasValue = true;
+        }
+
+        // ma -> (a -> b) -> mb
+        public Maybe<TResult> Select<TResult>(Func<T, TResult> function)
+        {
+            if (function == null)
+                throw new ArgumentNullException(nameof(function));
+            if (_hasValue)
+                return new Maybe<TResult>(function(_value));
+            return new Maybe<TResult>();
+        }
+
+        // ma -> (a -> mb) -> mb
+        public Maybe<TResult> SelectMany<TResult>(Func<T, Maybe<TResult>> function)
+        {
+            if (function == null)
+                throw new ArgumentNullException(nameof(function));
+            if (_hasValue)
+                return function(_value);
+            return new Maybe<TResult>();
+        }
+
+        // ma -> b -> (a -> b) -> b
+        public TResult Match<TResult>(TResult nothing, Func<T, TResult> just)
+        {
+            if (just == null)
+                throw new ArgumentNullException(nameof(just));
+            return _hasValue ? just(_value) : nothing;
+        }
+                
+        // ma -> (a -> tb) -> tmb
+        public async Task<Maybe<TResult>> AwaitSideEffect<TResult>(Func<T, Task<TResult>> function)
+        {
+            if (function == null)
+                throw new ArgumentNullException(nameof(function));
+            if (!_hasValue)
+                return new Maybe<TResult>();
+            var result = await function(_value);
+            return result == null ?
+                new Maybe<TResult>() :
+                new Maybe<TResult>(result);
+        }
+    }
+}
