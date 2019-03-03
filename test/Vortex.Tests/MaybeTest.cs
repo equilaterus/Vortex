5d4a88bd6fbd7c81d2ff83f0643f6ea8a04de25d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-03-02 22:03:27 -0500

    Adding tests for Maybe (WIP)

---
+++ test/Vortex.Tests/MaybeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Equilaterus.Vortex.Tests
+{
+    public class Maybe
+    {
+        [Fact]
+        public void Create_Null_ThrowsError()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => { Maybe<object> maybe = new Maybe<object>(null); });
+		}
+    }
+}
