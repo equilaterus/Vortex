18275b4e31825b33a5004a5091c71b954a26e990
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-03-02 23:07:43 -0500

    Adding Maybe tests (WIP)

--- test/Vortex.Tests/MaybeTest.cs
+++ test/Vortex.Tests/MaybeTest.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Vortex.Tests.Shared;
 using Xunit;
 
 namespace Equilaterus.Vortex.Tests
 {
     public class Maybe
     {
+        const string HAS_VALUE_FIELD = "_hasValue";
+        const string VALUE_FIELD = "_value";
+
+        [Fact]
+        public void Create_Empty_Success()
+        {
+            Maybe<object> maybe = new Maybe<object>();
+
+            Assert.False(maybe.GetPrivateField<bool>(HAS_VALUE_FIELD));
+            Assert.Null(maybe.GetPrivateField<object>(VALUE_FIELD));
+        }
+
         [Fact]
         public void Create_Null_ThrowsError()
         {
