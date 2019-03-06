38c03290f4a5107bd2d7ac89256783655ba6ffdf
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-03-05 20:57:05 -0500

    Adding TaskExtension tests (WIP)
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ test/Vortex.Tests/TaskExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Vortex.Tests
+{
+    public class TaskExtensionsTest
+    {
+        [Fact]
+        public async Task Select_NullTask_ThrowsError()
+        {
+            // Execute and check
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => Equilaterus.Vortex.TaskExtensions.Select<object, object>(null, t => t)
+            );
+            
+            Assert.Equal("source", exception.ParamName);
+        }
+
+        [Fact]
+        public async Task Select_NullSelector_ThrowsError()
+        {
+            // Execute and check
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => Equilaterus.Vortex.TaskExtensions.Select<object, object>(Task.FromResult<object>(null), null)
+            );
+
+            Assert.Equal("selector", exception.ParamName);
+        }
+    }
+}
