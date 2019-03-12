933ca0a56723045513c887524b32660e72abbadb
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-03-11 20:24:34 -0500

    Added Select Many with Selector tests for TaskExtensions
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- test/Vortex.Tests/TaskExtensionsTest.cs
+++ test/Vortex.Tests/TaskExtensionsTest.cs
@@ -79,5 +79,49 @@ namespace Equilaterus.Vortex.Tests
             Assert.Equal("selector", exception.ParamName);
         }
 
+        [Fact]
+        public async Task SelectManyWithSelector_Success()
+        {
+            // Prepare and execute
+            var result = await TaskExtensions.SelectMany(
+                AsyncMethod(), str => Task.FromResult(str[0]), (str, c) => str + c);
+
+            // Check
+            Assert.Equal("VortexV", result);
+        }
+
+        [Fact]
+        public async Task SelectManyWithSelector_FromNotation_Success()
+        {
+            // Prepare and execute
+            var result = await from i in AsyncMethod()
+                               from b in AsyncMethod()
+                               select i + b;
+
+            // Check
+            Assert.Equal("VortexVortex", result);
+        }
+
+        [Fact]
+        public async Task SelectManyWithSelector_NullTask_ThrowsError()
+        {
+            // Execute and check
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => TaskExtensions.SelectMany<object, object>(null, t => Task.FromResult(t))
+            );
+
+            Assert.Equal("source", exception.ParamName);
+        }
+
+        [Fact]
+        public async Task SelectManyWithSelector_NullSelector_ThrowsError()
+        {
+            // Execute and check
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => TaskExtensions.SelectMany<object, object>(Task.FromResult<object>(null), null)
+            );
+
+            Assert.Equal("selector", exception.ParamName);
+        }
     }
 }
