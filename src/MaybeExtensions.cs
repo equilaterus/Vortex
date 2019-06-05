859404cd0e8c71c19ea4b6738301571845a211b1
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-06-05 11:49:49 -0500

    Added Maybe Extensions
    closes #109

---
+++ src/MaybeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equilaterus.Vortex
+{
+    public static class MaybeExtensions
+    {
+        public static TResult MatchBool<TResult>(this Maybe<bool> maybe, Func<bool, TResult> just, TResult nothing)
+        {
+            if (just == null)
+                throw new ArgumentNullException(nameof(just));
+            bool value = maybe.Match(t => t, false);
+            return value ? just(value) : nothing;
+        }
+    }
+}
