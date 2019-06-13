58d50ba4ee48767a048f44e2465e2a4f12f9b563
Hugo Rodriguez <harf2310@gmail.com>
2019-06-13 07:24:47 -0500

    Create MatchBool first test
    Implement false data scenario

---
+++ test/MaybeExtensionTest.cs
@@ -0,0 +1,25 @@
+﻿using Equilaterus.Vortex;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Vortex.Tests
+{
+    public class MaybeExtensionTest
+    {
+        [Fact]
+        public void MatchBool_False()
+        {
+            //Prepare
+            Maybe<bool> maybe = new Maybe<bool>();
+
+            //Execute
+            var result = maybe.MatchBool(x => x == true, false);
+
+            //Check
+            Assert.False(result);
+        }
+
+    }
+}
