4bfbca3807288a9070559aa0cd91356626e4915b
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-14 13:54:00 -0500

    Updating AsMaybe tests

--- test/Helpers/ObjectExtensionTest.cs
+++ test/Helpers/ObjectExtensionTest.cs
@@ -8,7 +8,7 @@ namespace Equilaterus.Vortex.Helpers.Tests
     public class ObjectExtensionTest
     {
         [Fact]
-        public static void AsMaybe_Null_Returns_Nothing()
+        public static void Nullable_AsMaybe_Null_Returns_Nothing()
         {
             // Prepare
             int? str = null;
@@ -22,22 +22,7 @@ namespace Equilaterus.Vortex.Helpers.Tests
         }
 
         [Fact]
-        public static void AsMaybeString_Null_Returns_Nothing()
-        {
-            // Validating with a different data-type 
-            // Prepare
-            string str = null;
-
-            // Execute
-            Maybe<string> maybe = str.AsMaybe();
-            var result = maybe.Match(x => x, string.Empty);
-
-            // Check
-            Assert.Equal(string.Empty, result);
-        }
-
-        [Fact]
-        public static void AsMaybe_Returns_Just()
+        public static void Nullable_AsMaybe_Returns_Just()
         {
             // Prepare
             int? number = 700;
@@ -51,7 +36,36 @@ namespace Equilaterus.Vortex.Helpers.Tests
         }
 
         [Fact]
-        public static void AsMaybe_Object_Returns_Just()
+        public static void Class_AsMaybe_Null_Returns_Nothing()
+        {
+            // Prepare
+            string str = null;
+
+            // Execute
+            Maybe<string> maybe = str.AsMaybe();
+            var result = maybe.Match(x => x, string.Empty);
+
+            // Check
+            Assert.Equal(string.Empty, result);
+        }
+
+        [Fact]
+        public static void Class_AsMaybe_Returns_Just()
+        {
+            // Using polymorphism
+            // Prepare
+            string str = "Vortex!";
+
+            // Execute
+            Maybe<string> maybe = str.AsMaybe();
+            var result = maybe.Match(x => x, string.Empty);
+
+            // Check
+            Assert.Equal("Vortex!", result);
+        }
+
+        [Fact]
+        public static void Class_AsMaybe_Object_Returns_Just()
         {
             // Using polymorphism
             // Prepare
@@ -59,7 +73,7 @@ namespace Equilaterus.Vortex.Helpers.Tests
 
             // Execute
             Maybe<object> maybe = str.AsMaybe<object>();
-            var result = maybe.Match(x => x, -1);
+            var result = maybe.Match(x => x, string.Empty);
 
             // Check
             Assert.Equal("Vortex!", result);
