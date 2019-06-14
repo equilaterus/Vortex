361e7c621440a8d7f3e47398b5709f5a4aeaf71a
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-13 23:52:16 -0500

    Adding AsMaybe tests
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ test/Helpers/ObjectExtensionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Equilaterus.Vortex.Helpers.Tests
+{
+    public class ObjectExtensionTest
+    {
+        [Fact]
+        public static void AsMaybe_Null_Returns_Nothing()
+        {
+            // Prepare
+            int? str = null;
+
+            // Execute
+            Maybe<int?> maybe = str.AsMaybe();
+            var result = maybe.Match(x => x, -1);
+
+            // Check
+            Assert.Equal(-1, result);
+        }
+    }
+}
