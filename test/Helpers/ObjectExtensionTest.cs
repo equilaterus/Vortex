0c3ab2e549f4c713f4baef94d2868a12b56397b2
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-13 23:52:59 -0500

    Adding more tests for AsMaybe
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- test/Helpers/ObjectExtensionTest.cs
+++ test/Helpers/ObjectExtensionTest.cs
@@ -20,5 +20,49 @@ namespace Equilaterus.Vortex.Helpers.Tests
             // Check
             Assert.Equal(-1, result);
         }
+
+        [Fact]
+        public static void AsMaybeString_Null_Returns_Nothing()
+        {
+            // Validating with a different data-type 
+            // Prepare
+            string str = null;
+
+            // Execute
+            Maybe<string> maybe = str.AsMaybe();
+            var result = maybe.Match(x => x, string.Empty);
+
+            // Check
+            Assert.Equal(string.Empty, result);
+        }
+
+        [Fact]
+        public static void AsMaybe_Returns_Just()
+        {
+            // Prepare
+            int? number = 700;
+
+            // Execute
+            Maybe<int?> maybe = number.AsMaybe();
+            var result = maybe.Match(x => x, -1);
+
+            // Check
+            Assert.Equal(700, result);
+        }
+
+        [Fact]
+        public static void AsMaybe_Object_Returns_Just()
+        {
+            // Using polymorphism
+            // Prepare
+            string str = "Vortex!";
+
+            // Execute
+            Maybe<object> maybe = str.AsMaybe<object>();
+            var result = maybe.Match(x => x, -1);
+
+            // Check
+            Assert.Equal("Vortex!", result);
+        }
     }
 }
