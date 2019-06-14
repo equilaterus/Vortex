2f465251d1f8f36945d5144bfd88c80d0ce7f26a
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-14 13:53:43 -0500

    Splitting object extensions for Nullables and for Classes

--- src/Helpers/ObjectExtensions.cs
+++ src/Helpers/ObjectExtensions.cs
@@ -6,7 +6,15 @@ namespace Equilaterus.Vortex.Helpers
 {
     public static class ObjectExtensions
     {
-        public static Maybe<T> AsMaybe<T>(this T obj)
+        public static Maybe<T?> AsMaybe<T>(this T? obj) where T : struct
+        {
+            if (obj is null)
+                return new Maybe<T?>();
+            else
+                return new Maybe<T?>((T)obj);
+        }
+
+        public static Maybe<T> AsMaybe<T>(this T obj) where T : class
         {
             if (obj is null)
                 return new Maybe<T>();
