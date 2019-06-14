983f0c60646aefa5ac2152ffe17d87a58d330e94
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-13 23:51:50 -0500

    Adding Object Extensions -> AsMaybe
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/Helpers/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equilaterus.Vortex.Helpers
+{
+    public static class ObjectExtensions
+    {
+        public static Maybe<T> AsMaybe<T>(this T obj)
+        {
+            if (obj is null)
+                return new Maybe<T>();
+            else
+                return new Maybe<T>(obj);
+        }
+    }
+}
